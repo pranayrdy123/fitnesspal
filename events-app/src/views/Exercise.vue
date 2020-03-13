bdc85b24bc5090b754cda10044313113b0bb93a0
pranay kumar reddy <lotlap1@hawkmail.newpaltz.edu>
2020-03-12 21:46:00 -0400

    notfinal

---
+++ events-app/src/views/Exercise.vue
@@ -0,0 +1,35 @@
+ <<template>
+     
+ 
+ <div class="field-me has-addons">
+  <div class="control">
+    <p>Search an exercise from our database by name</p>
+    <input class="input" type="text" placeholder="Find an Exercise">
+  </div>
+  <div class="control">
+    <a class="button is-info">
+      Search
+    </a>
+  </div>
+
+<div>
+  <div class="select is-rounded">
+  <select>
+    <option>Rounded dropdown</option>
+    <option>With options</option>
+  </select>
+</div>
+<div>
+<figure class="image is-sm">
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81KjDc1kVAL._AC_SL1500_.jpg">
+</figure>
+</div>
+</div>
+</div>
+</template>
+<style scoped>
+.field-me{
+  align-content: center;
+}
+</style>>
+
